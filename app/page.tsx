7c0b2f95116c001181b68924706609afa2c249b8
Travis Menghini <menghinitravis@gmail.com>
2024-12-09 19:52:02 -0600

    Uses ReactECharts Instead of recharts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-'use client';
-import { Button, TextField } from '@mui/material';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts';
+'use client'
+import React from 'react';
 import { useState } from 'react';
-import { Typography } from '@mui/material';
-  
+import ReactECharts from 'echarts-for-react';
+import { Button, TextField, Typography } from '@mui/material';
+
 export default function Home() {
   const [apiLink, setApiLink] = useState('');
   const [data, setData] = useState([]);
@@ -27,6 +27,21 @@ export default function Home() {
       ...originCounts,
     };
   });
+  const monthlyOption = {
+    xAxis: {
+      type: 'category',
+      data: monthlyPlays.map((item) => item.month),
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: monthlyPlays.map((item) => item.totalPlays),
+        type: 'line',
+      },
+    ],
+  };
   const hourlyPlays = Array.from({ length: 24 }, (_, hour) => {
     const filteredData = data.filter((item: { time: number }) => {
       const date = new Date(item.time * 1000);
@@ -47,7 +62,21 @@ export default function Home() {
       ...originCounts,
     };
   });
-
+  const hourlyOption = {
+    xAxis: {
+      type: 'category',
+      data: hourlyPlays.map((item) => item.hour),
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: hourlyPlays.map((item) => item.totalPlays),
+        type: 'line',
+      },
+    ],
+  };
 
   const handleButtonClick = () => {
     console.log(apiLink);
@@ -66,71 +95,20 @@ export default function Home() {
       .catch(error => console.error('Error fetching data:', error));
       console.log(monthlyPlays);
   };
+  
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
       <Typography variant="h6" component="p">
         Welcome to Maloja Wrapped! You have listened to {data.length} songs in 2024.
       </Typography>
-
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', gap: '16px' }}>
-        <Typography variant="h6" component="p">
+      <Typography variant="h6" component="p">
           Play Count by Month
-        </Typography>
-
-        <ResponsiveContainer width="48%" height={300}>
-          <LineChart
-          data={monthlyPlays}
-          margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-          >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="month">
-          <Label value="Months" offset={-2} position="insideBottom" />
-        </XAxis>
-        <YAxis>
-          <Label value="Play Count" angle={-90} offset={0} position="insideLeft" />
-        </YAxis>
-        <Tooltip />
-        <Legend />
-        {Object.keys(monthlyPlays.reduce((acc, cur) => ({ ...acc, ...cur }), {})).filter(key => key !== 'month' && key !== 'total').map((key, index) => (
-          <Line key={index} type="monotone" dataKey={key} stroke={`#${Math.floor(Math.random()*16777215).toString(16)}`} />
-        ))}
-          </LineChart>
-        </ResponsiveContainer>
-
-        <Typography variant="h6" component="p">
+      </Typography>
+      <ReactECharts option={monthlyOption} style={{height: '500px'}}/>
+      <Typography variant="h6" component="p">
           Play Count by Hour of Day
-        </Typography>
-        <ResponsiveContainer width="48%" height={300}>
-          <LineChart
-        data={hourlyPlays}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-          >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="hour">
-          <Label value="Hour of Day" offset={-2} position="insideBottom" />
-        </XAxis>
-        <YAxis>
-          <Label value="Play Count" angle={-90} offset={0} position="insideLeft" />
-        </YAxis>
-        <Tooltip />
-        <Legend />
-        {Object.keys(monthlyPlays.reduce((acc, cur) => ({ ...acc, ...cur }), {})).filter(key => key !== 'month' && key !== 'total').map((key, index) => (
-          <Line key={index} type="monotone" dataKey={key} stroke={`#${Math.floor(Math.random()*16777215).toString(16)}`} />
-        ))}
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-
+      </Typography>
+      <ReactECharts option={hourlyOption} style={{height: '500px'}}/>
       <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
         {/*This TextField does nothing right now. The URL right now is set in next.config.ts*/}
         <TextField
@@ -144,4 +122,5 @@ export default function Home() {
       </div>
     </div>
   );
+  
 }
